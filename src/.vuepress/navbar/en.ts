fd5e78f0b4b2a9a2db8987f551f783d3d06cb020
Camcute123 <rw757@cam.ac.uk>
2023-11-25 23:32:09 +0000

    1

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -2,4 +2,26 @@ import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
   "/" , //主页
+  {
+    text: "主页", //指定名称
+    icon: "----",//指定图标
+    link: "/",
+  },
+
+  {
+    text:"111",
+    icon:"C",
+    children:[
+      "/",
+      {text:"222", icon:"home", link:"#"},
+      {
+        text:"333",
+        icon:"home",
+        children:[
+          {text:"444", icon:"home", link:"#"},
+          {text:"444", icon:"home", link:"#"}
+        ]
+      }
+    ]
+  }
 ]);

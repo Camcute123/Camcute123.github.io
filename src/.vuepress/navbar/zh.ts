db21d5e0f5027e8452d8cdea4629548a2d32aae1
Camcute123 <rw757@cam.ac.uk>
2023-12-16 22:19:43 +0000

    1

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -3,28 +3,36 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   "/zh/",
 
-  {
-    text:"内容",
-    icon:"Home-copy",
-    children:[
-      {
-        text:"学习笔记",
-        icon:"home",
-        children:[
-          {text:"实验报告", icon:"baogao1", link:"#"},
-          {text:"Python笔记", icon:"a-bijibenbiji", link:"#"},
-        ]
-      },
 
-      {
-        text:"日常分享",
-        icon:"home",
-        children:[
-          {text:"电影观感", icon:"yuedu1", link:"#"},
-          {text:"文章赏析", icon:"yuedu1", link:"#"},
-          {text:"摄影作品", icon:"-dianyingpiao-", link:"#"},
-        ]
-      }
+  { text:"学习笔记",
+    icon:"home",
+    children:[
+      {text:"电学", icon:"baogao1", link:"#"},
+      {text:"机械学", icon:"a-bijibenbiji", link:"#"},
+      {text:"结构学", icon:"a-bijibenbiji", link:"#"},
+      {text:"数学", icon:"a-bijibenbiji", link:"#"},
     ]
-  }
-]);
+  },
+
+
+  {
+    text:"python",
+    icon:"home",
+    children:[
+      {text:"笔记", icon:"yuedu1", link:"#"},
+      {text:"练习", icon:"yuedu1", link:"#"},
+      {text:"项目", icon:"-dianyingpiao-", link:"#"},
+    ]
+  },
+
+  {
+    text:"日常分享",
+    icon:"home",
+    children:[
+      {text:"电影观感", icon:"yuedu1", link:"#"},
+      {text:"文章赏析", icon:"yuedu1", link:"#"},
+      {text:"摄影作品", icon:"-dianyingpiao-", link:"#"},
+    ]
+  },
+])
+

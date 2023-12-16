da5af81e543bd5c2b405281630e0b031237d5888
Camcute123 <147645141+Camcute123@users.noreply.github.com>
2023-12-16 22:20:33 +0000

    Deploying to gh-pages from @ Camcute123/Camcute123.github.io@db21d5e0f5027e8452d8cdea4629548a2d32aae1 🚀

--- assets/pageview-qpbf9t_M.js
+++
@@ -1,7 +0,0 @@
-import{a6 as o}from"./app-lw52lRVx.js";var r={provider:"Waline",dark:"auto",serverURL:"https://comment.vervevividness.com/",reaction:["/Waline/emoji/bmoji_heartbeat.png","/Waline/emoji/bmoji_give_love.png","/Waline/emoji/bmoji_hard.png","/Waline/emoji/bmoji_emm.png","/Waline/emoji/bmoji_jiong.png","/Waline/emoji/bmoji_heiehi.png"],comment:!0,pageview:!0,copyright:!1};const n=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-lw52lRVx.js").then(i=>i.a7),__vite__mapDeps([]));return e({serverURL:r.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{n as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
